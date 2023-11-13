5a2830cced86175507e9f3026eabe15b759f1843
OctetularZ <taiabayomi15@gmail.com>
2023-11-13 20:22:27 +0000

    minor changes

--- index.js
+++ index.js
@@ -27,3 +27,21 @@ let observer_text = new IntersectionObserver(entries => {
 });
 
 observer_text.observe(document.querySelector('.middle'));
+
+
+// const handleOnMouseMove = e => {
+//   const {
+//     currentTarget: target
+//   } = e;
+
+//   const rect = target.getBoundingClientRect(),
+//     x = e.clientX - rect.left,
+//     y = e.clientY - rect.top;
+
+//   target.style.setProperty('--mouse-x', `${x}px`);
+//   target.style.setProperty('--mouse-y', `${y}px`);
+// }
+
+// for (const box of document.querySelectorAll('.middle-boxes')) {
+//   box.onmousemove = e => handleOnMouseMove(e)
+// }
